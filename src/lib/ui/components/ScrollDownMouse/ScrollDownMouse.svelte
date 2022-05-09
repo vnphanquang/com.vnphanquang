416cccb7812ec91f92d2f69965a5da2dee2cfcb0
Quang Phan <vnphanquang@gmail.com>
2022-05-09 22:50:18 +0700

    feat(components): ScrollDownMouse (animated, fixed size for now)

---
+++ src/lib/ui/components/ScrollDownMouse/ScrollDownMouse.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  export let id: string | undefined = undefined;
+  export let role: 'static' | 'button' = 'static';
+
+  const containerClass = `w-8 h-12 rounded-3xl border-2 flex justify-center border-current pt-2 text-fg ${$$props.class}`;
+  const scrollerClass = 'scroller bg-current';
+</script>
+
+{#if role === 'button'}
+  <button class={containerClass} type="button" on:click {id}>
+    <span class={scrollerClass} />
+  </button>
+{:else}
+  <div class={containerClass} {id}>
+    <span class={scrollerClass} />
+  </div>
+{/if}
+
+<style>
+  .scroller {
+    margin-right: 1px;
+    width: 3px;
+    height: 8px;
+    border-radius: 25%;
+    animation-name: scroll;
+    animation-duration: 2.2s;
+    animation-timing-function: cubic-bezier(0.15, 0.41, 0.69, 0.94);
+    animation-iteration-count: infinite;
+  }
+
+  @keyframes scroll {
+    0% {
+      opacity: 0;
+    }
+    10% {
+      transform: translateY(0);
+      opacity: 1;
+    }
+    100% {
+      transform: translateY(10px);
+      opacity: 0;
+    }
+  }
+</style>
---
+++ src/lib/ui/components/ScrollDownMouse/index.ts
@@ -0,0 +1,1 @@
+export { default as ScrollDownMouse } from './ScrollDownMouse.svelte';
