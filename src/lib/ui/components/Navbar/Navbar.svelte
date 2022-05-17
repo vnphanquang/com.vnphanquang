a1933fb1d532bbed09cf18d00376287b7a01adc9
Quang Phan <vnphanquang@gmail.com>
2022-05-17 17:47:45 +0700

    feat(navbar): add lightbulbtoggler

--- src/lib/ui/components/Navbar/Navbar.svelte
+++ src/lib/ui/components/Navbar/Navbar.svelte
@@ -1,6 +1,10 @@
 <script>
   import classnames from 'classnames';
-  import { fade } from 'svelte/transition';
+  import Icon from 'svelte-awesome/components/Icon.svelte';
+  import lightbulb from 'svelte-awesome/icons/lightbulbO';
+  import moon from 'svelte-awesome/icons/moonO';
+  import { elasticInOut } from 'svelte/easing';
+  import { fade, fly } from 'svelte/transition';
 
   import { HamburgerBtn } from '$lib/ui/components/HamburgerBtn';
 
@@ -19,6 +23,8 @@
       text: 'about',
     },
   };
+
+  let isLightTheme = true;
 </script>
 
 <nav
@@ -66,7 +72,32 @@
       </ul>
     {/if}
 
-    <ul class="flex items-center justify-end" />
+    <ul class="flex items-center justify-end">
+      <input type="checkbox" id="theme-toggler" bind:checked={isLightTheme} hidden />
+      <label class="cursor-pointer hover:text-primary" for="theme-toggler">
+        <span class="light">
+          <Icon data={moon} scale={2} />
+        </span>
+        <span class="dark">
+          <Icon data={lightbulb} scale={2} />
+        </span>
+      </label>
+    </ul>
+
+    {#if !isLightTheme}
+      <div transition:fade={{ duration: 800 }} class="backdrop fixed inset-0" />
+      <label
+        for="theme-toggler"
+        transition:fly={{ y: -500, easing: elasticInOut, duration: 1000 }}
+        class="fixed top-[10vh] left-1/2 -translate-x-1/2 cursor-pointer"
+      >
+        <div class="wire" />
+        <div class="bulb on">
+          <span />
+          <span />
+        </div>
+      </label>
+    {/if}
   </div>
 </nav>
 
@@ -78,5 +109,130 @@
 
   .navlink:hover::after {
     @apply origin-left scale-x-100;
+  }
+
+  label[for='theme-toggler'] {
+    @apply grid place-content-center;
+  }
+  #theme-toggler:checked ~ label[for='theme-toggler'] {
+    filter: drop-shadow(0 0 14px #ffad00);
+  }
+
+  label[for='theme-toggler'] > .light,
+  label[for='theme-toggler'] > .dark {
+    @apply col-start-1 row-start-1;
+    @apply duration-300 ease-in-out;
+    transition-property: transform, opacity;
+  }
+  #theme-toggler:checked ~ label[for='theme-toggler'] > .light {
+    @apply rotate-0 opacity-100;
+  }
+  #theme-toggler:checked ~ label[for='theme-toggler'] > .dark {
+    @apply rotate-45 opacity-0;
+  }
+
+  #theme-toggler:not(:checked) ~ label[for='theme-toggler'] > .light {
+    @apply -rotate-45 opacity-0;
+  }
+  #theme-toggler:not(:checked) ~ label[for='theme-toggler'] > .dark {
+    @apply rotate-0 opacity-100;
+  }
+
+  .bulb {
+    position: relative;
+    width: 80px;
+    height: 80px;
+    background: rgb(107, 107, 107);
+    border-radius: 50%;
+    z-index: 2;
+  }
+  .bulb.on {
+    background: #fff5b6;
+    box-shadow: 0 0 50px #fff5b6, 0 0 100px #fff5b6, 0 0 150px #fff5b6, 0 0 200px #fff5b6,
+      0 0 250px #fff5b6, 0 0 300px #fff5b6, 0 0 350px #fff5b6;
+  }
+
+  .bulb::before {
+    content: '';
+    position: absolute;
+    top: -50px;
+    left: 22.5px;
+    width: 35px;
+    height: 80px;
+    background: rgb(107, 107, 107);
+    border-top: 30px solid theme('colors.fg');
+    border-radius: 10px;
+  }
+
+  .bulb.on::before {
+    background: #fff5b6;
+  }
+
+  .bulb.on::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 120px;
+    height: 120px;
+    background: #fff5b6;
+    border-radius: 50%;
+    filter: blur(40px);
+  }
+
+  .bulb span:nth-child(1) {
+    position: absolute;
+    top: -16px;
+    left: -4px;
+    display: block;
+    width: 30px;
+    height: 30px;
+    background: transparent;
+    transform: rotate(342deg);
+    border-bottom-right-radius: 40px;
+    box-shadow: 20px 20px 0 10px rgb(107, 107, 107);
+  }
+
+  .bulb.on span:nth-child(1) {
+    box-shadow: 20px 20px 0 10px #fff5b6;
+  }
+
+  .bulb span:nth-child(2) {
+    position: absolute;
+    top: -16px;
+    right: -4px;
+    display: block;
+    width: 30px;
+    height: 30px;
+    background: transparent;
+    transform: rotate(17deg);
+    border-bottom-left-radius: 40px;
+    box-shadow: -20px 20px 0 10px rgb(107, 107, 107);
+  }
+
+  .bulb.on span:nth-child(2) {
+    box-shadow: -20px 20px 0 10px #fff5b6;
+  }
+
+  .wire {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: 100%;
+    width: 4px;
+    height: 20vh;
+    background: black;
+    z-index: 1;
+  }
+
+  .backdrop {
+    content: '';
+    pointer-events: none;
+    background: radial-gradient(
+      circle farthest-corner at 50vw 20vh,
+      rgba(0, 0, 0, 0.5) 0%,
+      rgba(0, 0, 0, 1) 85%
+    );
   }
 </style>
