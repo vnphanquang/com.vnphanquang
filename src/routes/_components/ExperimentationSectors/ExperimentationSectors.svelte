0d1a020481e74c8344b9197aa5f8cd5656dbbec4
Quang Phan <vnphanquang@gmail.com>
2022-05-18 22:46:59 +0700

    refactor(ExperimentationSectors): extract content to constant, reuse markup

--- src/routes/_components/ExperimentationSectors/ExperimentationSectors.svelte
+++ src/routes/_components/ExperimentationSectors/ExperimentationSectors.svelte
@@ -1,5 +1,48 @@
 <script lang="ts">
   import { SkewedFlipCard } from '$lib/ui/components';
+
+  const sectors = [
+    {
+      id: 'open-source',
+      title: ['open', 'source'],
+      headline: 'Broken applications, unfinished features, failed tests, ...',
+      paragraphs: [
+        'This is where my code never see the light of day.',
+        `Visit <a
+          href="https://github.com/vnphanquang"
+          class="c-link"
+          target="_blank">github</a
+        > for "free" softwares.`,
+      ],
+    },
+    {
+      id: 'blog-posts',
+      title: ['blog', 'posts'],
+      headline: 'Cringeworthy writing, irrational logics, irrelevant topics, ...',
+      paragraphs: [
+        'This is what I\'ll definitely regret two years from now.',
+        `See <a href="/blog" class="c-link">blog page</a> for more.`,
+      ],
+    },
+    {
+      id: 'captured-photos',
+      title: ['captured', 'photos'],
+      headline: 'Underexposed photos, unbalanced composition, out-of-focus subjects,...',
+      paragraphs: [
+        'Maybe one of these photo is worth a banh mi?',
+        `See <a href="/gallery" class="c-link">gallery</a> for (perhaps) joy?`,
+      ],
+    },
+    {
+      id: 'more-things',
+      title: ['more', 'things'],
+      headline: '',
+      paragraphs: [
+        `Absolutely nothing here. Maybe the possibilities are infinite? Perhaps it reflects my
+        empty soul. But really it's just here to balance this UI grid... /shrug`,
+      ],
+    },
+  ];
 </script>
 
 <div class="w-full max-w-5xl {$$props.class}">
@@ -8,67 +51,21 @@
     <p class="mt-4 italic">Projects go boom 💥</p>
   </div>
   <div class="mt-20 grid grid-cols-1 place-items-center gap-28 md:grid-cols-2">
-    <article>
-      <SkewedFlipCard class="h-64 w-80" id="open-source" skew="right">
-        <h3 slot="front" class="font-quang text-4xl">
-          <span class="enclosed">open</span>source
-        </h3>
-        <div slot="back" class="prose p-8 text-sm">
-          <p class="text-lg">Broken applications, unfinished features, failed tests, ...</p>
-          <p class="">This is where my code never see the light of day.</p>
-          <p class="">
-            Visit <a
-              href="https://github.com/vnphanquang"
-              class="underline hover:text-secondary"
-              target="_blank">github</a
-            > for "free" softwares.
-          </p>
-        </div>
-      </SkewedFlipCard>
-    </article>
-    <article>
-      <SkewedFlipCard class="h-64 w-80" id="something" skew="left">
-        <h3 slot="front" class="font-quang text-4xl">
-          <span class="enclosed">blog</span>posts
-        </h3>
-        <div slot="back" class="prose p-8 text-sm">
-          <p class="text-lg">Cringeworthy writing, irrational logics, irrelevant topics, ...</p>
-          <p class="">This is what I'll definitely regret two years from now.</p>
-          <p class="">
-            See <a href="/blog" class="underline hover:text-secondary">blog page</a> for more.
-          </p>
-        </div>
-      </SkewedFlipCard>
-    </article>
-    <article>
-      <SkewedFlipCard class="h-64 w-80 p-4" id="gallery" skew="right">
-        <h3 slot="front" class="font-quang text-4xl">
-          <span class="enclosed">captured</span>photos
-        </h3>
-        <div slot="back" class="prose p-8 text-sm">
-          <p class="text-lg">
-            Underexposed photos, unbalanced composition, out-of-focus subjects,...
-          </p>
-          <p class="">Maybe one of these photo is worth a banh mi?</p>
-          <p class="">
-            See <a href="/gallery" class="underline hover:text-secondary">gallery</a> for (perhaps) joy?
-          </p>
-        </div>
-      </SkewedFlipCard>
-    </article>
-    <article>
-      <SkewedFlipCard class="h-64 w-80" id="blog" skew="left">
-        <h3 slot="front" class="font-quang text-4xl">
-          <span class="enclosed">more</span>things
-        </h3>
-        <div slot="back" class="prose p-8 text-sm">
-          <p class="text-base">
-            Absolutely nothing here. Maybe the possibilities are infinite? Perhaps it reflects my
-            empty soul. But really it's just here to balance this UI grid... /shrug
-          </p>
-        </div></SkewedFlipCard
-      >
-    </article>
+    {#each sectors as { id, title, headline, paragraphs }, index}
+      <article>
+        <SkewedFlipCard class="w-72 h-64 md:w-80" {id} skew={index % 2 === 0 ? 'right' : 'left'}>
+          <h3 slot="front" class="font-quang text-4xl">
+            <span class="enclosed">{title[0]}</span>{title[1]}
+          </h3>
+          <div slot="back" class="prose p-4 md:p-8 text-sm">
+            <p class="text-lg">{headline}</p>
+            {#each paragraphs as paragraph}
+              <p>{@html paragraph}</p>
+            {/each}
+          </div>
+        </SkewedFlipCard>
+      </article>
+    {/each}
   </div>
 </div>
 
