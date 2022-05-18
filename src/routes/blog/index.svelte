d1e11ad56a300024fa2f220266dc989adec956bc
Quang Phan <vnphanquang@gmail.com>
2022-05-18 22:25:48 +0700

    chore: add placeholders for blog & gallery pages

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,1 +1,6 @@
-<h1>Blog</h1>
+<main class="grid flex-1 place-items-center gap-y-14 py-10">
+  <section class="font-quang text-center">
+    <h1 class="text-5xl font-bold">Yet Another Blog</h1>
+    <h2 class="text-2xl italic mt-6">Cmming soon...</h2>
+  </section>
+</main>
--- src/routes/gallery/index.svelte
+++ src/routes/gallery/index.svelte
@@ -1,1 +1,8 @@
-<h1>Gallery</h1>
+<main class="grid flex-1 place-items-center gap-y-14 py-10">
+  <section class="font-quang text-center">
+    <h1 class="text-5xl font-bold">Digital Memory Rectangles</h1>
+    <h2 class="text-2xl italic mt-6">Crappy photos I took and the story behind them</h2>
+    <h2 class="text-2xl italic mt-6">Coming soon...</h2>
+  </section>
+</main>
+
