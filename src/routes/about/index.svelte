01bcb020f5b6bc2cacd68586d9d6107be6b02f00
Quang Phan <vnphanquang@gmail.com>
2022-05-19 10:50:14 +0700

    fix(about): portrait static filepath

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -224,7 +224,7 @@
   >
     <h1 class="font-quang text-5xl font-bold">Quang Phan</h1>
     <h2 class="font-quang text-2xl italic">Learner, Developer, Jack of Zero Trade</h2>
-    <img src="/images/sketch_portrait.svg" alt="portrait" width="100" class="bg-bg" />
+    <img src="/images/vnphanquang/sketch_portrait.svg" alt="portrait" width="100" class="bg-bg" />
     <button id="resume-download" type="button" class="c-btn-outline py-1 px-2 text-xs uppercase">
       <a href="/QuangPhan_Resume.pdf" target="_blank"> Download PDF resume </a>
     </button>
