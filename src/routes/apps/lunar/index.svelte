98b39577573fe6e2dd8935fba682ac4432491cd4
Quang Phan <vnphanquang@gmail.com>
2022-06-04 08:01:42 +0700

    WIP: implement lunar conversion & ics generator with rrule

---
+++ src/routes/apps/lunar/index.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { convertLunar2Solar } from '$lib/third-party/lunar';
+
+  let month: number;
+  let date: number;
+
+  $: solar = convertLunar2Solar(date, month, 2022, 0, 7);
+</script>
+
+<p>{solar}</p>
+
+<input
+  min="1"
+  max="12"
+  type="number"
+  class="c-input"
+  bind:value={month}
+/>
+
+<input
+  min="1"
+  max="12"
+  type="number"
+  class="c-input"
+  bind:value={date}
+/>
