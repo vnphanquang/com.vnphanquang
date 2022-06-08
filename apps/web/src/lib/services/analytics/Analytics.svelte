876910aabebb28e5356f44595b828c7f4925729a
Quang Phan <vnphanquang@gmail.com>
2022-06-08 17:35:05 +0700

    chore: ignore dev env for google analytics

--- apps/web/src/lib/services/analytics/Analytics.svelte
+++ apps/web/src/lib/services/analytics/Analytics.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import { browser } from '$app/env';
   import { page } from '$app/stores';
+  import { AppConfig } from '$config';
 
   const MEASUREMENT_ID = 'G-8WJ72291KL';
 
-  $: if (browser && gtag) {
+  $: if (browser && AppConfig.mode === 'production' && gtag) {
     gtag('config', MEASUREMENT_ID, {
       page_title: document.title,
       page_location: $page.url.href,
@@ -14,13 +15,15 @@
 </script>
 
 <svelte:head>
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id={MEASUREMENT_ID}"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+  {#if AppConfig.mode === 'production'}
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={MEASUREMENT_ID}"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
-    gtag('config', 'G-8WJ72291KL');
-  </script>
+      gtag('config', 'G-8WJ72291KL');
+    </script>
+  {/if}
 </svelte:head>
