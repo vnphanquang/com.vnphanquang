b806a7d657c80cccc8c35774c89f21d1beddfedb
Quang Phan <vnphanquang@gmail.com>
2022-06-08 17:05:57 +0700

    feat: bootstrap Google Analytics

---
+++ apps/web/src/lib/services/analytics/Analytics.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { browser } from '$app/env';
+  import { page } from '$app/stores';
+
+  const MEASUREMENT_ID = 'G-8WJ72291KL';
+
+  $: if (browser && gtag) {
+    gtag('config', MEASUREMENT_ID, {
+      page_title: document.title,
+      page_location: $page.url.href,
+      page_path: $page.url.pathname,
+    });
+  }
+</script>
+
+<svelte:head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id={MEASUREMENT_ID}"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-8WJ72291KL');
+  </script>
+</svelte:head>
--- apps/web/src/routes/[locale=locale]/__layout-root.svelte
+++ apps/web/src/routes/[locale=locale]/__layout-root.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import BackToTopBtn from '$lib/components/BackToTopBtn/BackToTopBtn.svelte';
   import CommandPaletteWrapper from '$lib/components/CommandPalette/CommandPalette.wrapper.svelte';
+  import Analytics from '$lib/services/analytics/Analytics.svelte';
   import '$lib/styles/app.css';
 </script>
+
+<Analytics />
 
 <slot />
 
