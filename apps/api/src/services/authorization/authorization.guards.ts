f27dbdf487b4e43f74cfda09fceb4f8610c291d1
Quang Phan <vnphanquang@gmail.com>
2022-06-19 15:26:00 +0700

    chore(api): bootstrap basic rbac

---
+++ apps/api/src/services/authorization/authorization.guards.ts
@@ -0,0 +1,25 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { Role } from '@prisma/client';
+import { Observable } from 'rxjs';
+
+import { ROLES_KEY } from './authorization.decorators';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+    const user = GqlExecutionContext.create(context).getContext().req.user;
+    return requiredRoles.some((role) => user.role === role);
+  }
+}
