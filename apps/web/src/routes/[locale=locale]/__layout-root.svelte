14c1d5f16afe28d0cde67d1cff042628fe1296e9
Quang Phan <vnphanquang@gmail.com>
2022-08-01 21:30:12 +0700

    feat(web): global loader

--- apps/web/src/routes/[locale=locale]/__layout-root.svelte
+++ apps/web/src/routes/[locale=locale]/__layout-root.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { browser } from '$app/env';
   import { beforeNavigate } from '$app/navigation';
-  import { page } from '$app/stores';
+  import { navigating, page } from '$app/stores';
   import { AppRoutes } from '$generated/routing';
   import BackToTopBtn from '$lib/components/BackToTopBtn/BackToTopBtn.svelte';
   import CommandPaletteWrapper from '$lib/components/CommandPalette/CommandPalette.wrapper.svelte';
@@ -87,7 +87,11 @@
     <meta property="profile:username" content={username} />
   {/if}
 
-  <link rel="alternate" hreflang="x-default" href={locale === Locale.En ? originalUrl : originalUrl.replace(`/${locale}/`, '/en/')} />
+  <link
+    rel="alternate"
+    hreflang="x-default"
+    href={locale === Locale.En ? originalUrl : originalUrl.replace(`/${locale}/`, '/en/')}
+  />
   {#each langAlternatives.filter((le) => le !== locale) as l}
     <link rel="alternate" hreflang={l} href={originalUrl.replace(`/${locale}/`, `/${l}/`)} />
   {/each}
@@ -109,3 +113,98 @@
 
 <CommandPaletteWrapper />
 <BackToTopBtn />
+
+<!-- TODO: extract custom store and reuse for api loading -->
+{#if !!$navigating}
+  <div class="fixed z-overlay inset-0 grid place-items-center bg-bg-accent/90">
+    <!-- https://codepen.io/t_afif/pen/gOmooGQ -->
+    <div class="global-loader" />
+  </div>
+{/if}
+
+<style lang="postcss">
+  .global-loader {
+    @apply text-primary scale-125;
+
+    width: 40px;
+    height: 20px;
+    --c: radial-gradient(farthest-side, currentColor 93%, #0000);
+    background: var(--c) 0 0, var(--c) 50% 0;
+    background-size: 8px 8px;
+    background-repeat: no-repeat;
+    position: relative;
+    clip-path: inset(-200% -100% 0 0);
+    animation: loader-0 1.5s linear infinite;
+  }
+  .global-loader:before {
+    content: '';
+    position: absolute;
+    width: 8px;
+    height: 12px;
+    background: currentColor;
+    left: -16px;
+    top: 0;
+    animation: loader-1 1.5s linear infinite, loader-2 0.5s cubic-bezier(0, 200, 0.8, 200) infinite;
+  }
+  .global-loader:after {
+    content: '';
+    position: absolute;
+    inset: 0 0 auto auto;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background: currentColor;
+    animation: loader-3 1.5s linear infinite;
+  }
+
+  @keyframes loader-0 {
+    0%,
+    30% {
+      background-position: 0 0, 50% 0;
+    }
+    33% {
+      background-position: 0 100%, 50% 0;
+    }
+    41%,
+    63% {
+      background-position: 0 0, 50% 0;
+    }
+    66% {
+      background-position: 0 0, 50% 100%;
+    }
+    74%,
+    100% {
+      background-position: 0 0, 50% 0;
+    }
+  }
+
+  @keyframes loader-1 {
+    90% {
+      transform: translateY(0);
+    }
+    95% {
+      transform: translateY(15px);
+    }
+    100% {
+      transform: translateY(15px);
+      left: calc(100% - 8px);
+    }
+  }
+
+  @keyframes loader-2 {
+    100% {
+      top: -0.1px;
+    }
+  }
+
+  @keyframes loader-3 {
+    0%,
+    80%,
+    100% {
+      transform: translate(0);
+    }
+    90% {
+      transform: translate(26px);
+    }
+  }
+</style>
